4449bf507caccc86c86000cb1d7c10d24aa3d5ae
sirineab <58654187+sirineab@users.noreply.github.com>
2019-12-12 03:49:28 +0100

    planning

--- planning/src/main/java/com/project/planning/planning/PlanningApplication.java
+++ planning/src/main/java/com/project/planning/planning/PlanningApplication.java
@@ -2,8 +2,10 @@ package com.project.planning.planning;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class PlanningApplication {
 
 	public static void main(String[] args) {
